ae2ce150d3f80649d4ffb8d50c3b6a2b484d8caf
Mario Rosales <marrr.17@icloud.com>
2026-02-25 11:59:09 -0700

    Added input validation and error handling

--- main.py
+++ main.py
@@ -1,11 +1,26 @@
 import requests
-url = "https://api.coingecko.com/api/v3/simple/price"
-params = {
-    "ids": "bitcoin",
-    "vs_currencies": "usd"
-}
-response = requests.get("https://api.coingecko.com/api/v3/simple/price")
-response = requests.get(url, params=params)
-data = response.json()
-price = data["bitcoin"]["usd"]
-print(f"bitcoin price: ${price}")
+
+
+def main():
+    url = "https://api.coingecko.com/api/v3/simple/price"
+    crypto = input(
+        'Name the crytpo youd like to research? Ex bitcoin,ethereum:').strip().lower()
+    params = {
+        "ids": crypto,
+        "vs_currencies": "usd"
+    }
+    response = requests.get(url, params=params)
+    if response.status_code != 200:
+        print("error fetching data")
+        return
+
+    data = response.json()
+    if crypto in data:
+        price = data[crypto]["usd"]
+        print(f"The price of {crypto} at the moment is: ${price}")
+    else:
+        print('sorry we do not support that token at the moment')
+
+
+if __name__ == "__main__":
+    main()

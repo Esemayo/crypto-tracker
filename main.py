af40070de19c1104cc36f6d27e85a219f8aad4a0
Mario Rosales <marrr.17@icloud.com>
2026-02-26 17:29:44 -0700

    Added fiat input and improved error handling

--- main.py
+++ main.py
@@ -1,26 +1,29 @@
 import requests
-
-
-def main():
+def get_price(coin, currency):
     url = "https://api.coingecko.com/api/v3/simple/price"
-    crypto = input(
-        'Name the crytpo youd like to research? Ex bitcoin,ethereum:').strip().lower()
     params = {
-        "ids": crypto,
-        "vs_currencies": "usd"
+        "ids": coin,
+        "vs_currencies": currency,
     }
     response = requests.get(url, params=params)
-    if response.status_code != 200:
-        print("error fetching data")
-        return
-
     data = response.json()
-    if crypto in data:
-        price = data[crypto]["usd"]
-        print(f"The price of {crypto} at the moment is: ${price}")
-    else:
-        print('sorry we do not support that token at the moment')
+    if response.status_code != 200:
+        return None, "error"
+    data = response.json()
+    if coin not in data or currency not in data[coin]: 
+        return None, "Error"  
+        
+    price = data[coin][currency]
+    return price, None
 
+def main():
+    crypto = input('Name the crypto youd like to research? Ex bitcoin,ethereum:').strip().lower()
+    currency = input("Which currency would you like price to be calculated?").strip().lower()
+    price, error = get_price(crypto, currency)
+    if error:
+        print(error)
+    else:
+        print(f"The price of {crypto} at the moment is: ${price}")
 
 if __name__ == "__main__":
     main()
